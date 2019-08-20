0794d097965f38214e5bdc8535f58e3a3a186f0e
sjohnson-CAGifts <54120577+sjohnson-CAGifts@users.noreply.github.com>
2019-08-20 16:20:49 -0500

    Add files via upload

---
+++ employees.js
@@ -0,0 +1,399 @@
+var staff = {
+	'administration': {
+    	"employees": [
+			{
+				"name": "Heinrich Johnsen",
+				"primary": "HEINRICH_JOHNSEN.jpg",
+				"secondary": null,
+				"department": "Administration",
+				"role": "President / Owner"
+			},
+			{
+				"name": "CJ Van Wagner",
+				"primary": "CJ.jpg",
+				"secondary": "CJ_2.jpg",
+				"department": "Administration",
+				"role": "VP of People and Product Development"
+			},
+			{
+				"name": "Michael Hoving",
+				"primary": "MIKE_HOVING.jpg",
+				"secondary": null,
+				"department": "Administration",
+				"role": "VP of Finance and Operations"
+			},
+			{
+				"name": "Eden Parker",
+				"primary": "EDEN_PARKER.jpg",
+				"secondary": "EDEN_PARKER_2.jpg",
+				"department": "Administration",
+				"role": "Bibles.net Coordinator"
+			},
+			{
+				"name": "Susan Middel",
+				"primary": "SUSAN_MIDDEL.jpg",
+				"secondary": null,
+				"department": "Administration",
+				"role": "Administrative Assistant"
+			},
+			{
+				"name": "Trevor Heinisch",
+				"primary": "TREVOR_HEINISCH.jpg",
+				"secondary": null,
+				"department": "Administration",
+				"role": "Information Technology Manager"
+	  		}
+	    ],
+    	"department": "Administration"
+  	},
+	'accounting': {
+    	"employees": [
+			{
+	        	"name": "Evelyn Szczepanik",
+	        	"primary": "EVELYN_SZXZEPANIK.jpg",
+	        	"secondary": null,
+	        	"department": "Accounting",
+	    		"role": "Accounts Receivable"
+			},
+			{
+				"name": "Lettie Van Wyk",
+				"primary": "LETTIE_VanWyk.jpg",
+				"secondary": null,
+				"department": "Accounting",
+				"role": "Accounting Assistant"
+			}
+    	],
+    	"department": "Accounting"
+	},
+	'creative': {
+    	"employees": [
+			{
+				"name": "Brad Miedema",
+				"primary": "BRAD_MIEDEMA.jpg",
+				"secondary": null,
+				"department": "Creative",
+				"role": "Art Director"
+			},
+			{
+				"name": "Allison Sowers",
+				"primary": "ALLISON_SOWERS.jpg",
+				"secondary": null,
+				"department": "Creative",
+				"role": "Designer"
+			},
+			{
+				"name": "Erin Simula",
+				"primary": "ERIN_SIMULA.jpg",
+				"secondary": null,
+				"department": "Creative",
+				"role": "Designer"
+			},
+			{
+				"name": "Nicole Grimes",
+				"primary": null,
+				"secondary": null,
+				"department": "Creative",
+				"role": "Designer"
+			},
+			{
+				"name": "Staci Bryant",
+				"primary": "STACI_BRYANT_2.jpg",
+				"secondary": null,
+				"department": "Creative",
+				"role": "Designer / Superwoman"
+			},
+			{
+				"name": "Greg Heck",
+				"primary": "GREG_HECK.jpg",
+				"secondary": null,
+				"department": "Creative",
+				"role": "Photographer"
+			}
+    	],
+    	"department": "Creative"
+	},
+	'customerService': {
+    	"employees": [
+			{
+				"name": "Christina Okesson",
+				"primary": "CHRISTINA_OKESSON.jpg",
+				"secondary": null,
+				"department": "Customer Service",
+				"role": "Customer Service Representative"
+			},
+			{
+				"name": "Erica Yoneyama",
+				"primary": "ERICA_YONEYAMA.jpg",
+				"secondary": null,
+				"department": "Customer Service",
+				"role": "Customer Service Representative"
+			},
+			{
+				"name": "Jim Snyder",
+				"primary": "JAMES_SNYDER.jpg",
+				"secondary": null,
+				"department": "Customer Service",
+				"role": "Customer Service Representative"
+			},
+			{
+				"name": "Kelly Glancey",
+				"primary": "KELLY_GLANCEY.jpg",
+				"secondary": null,
+				"department": "Customer Service",
+				"role": "Key Accounts Manager"
+			}
+    	],
+    	"department": "Customer Service"
+	},
+	'distribution': {
+    	"employees": [
+			{
+				"name": "Ron Wisser",
+				"primary": "RON_WISSER_2.jpg",
+				"secondary": "RON_WISSER.jpg",
+				"department": "Distribution",
+				"role": "Distribution Manager"
+			},
+			{
+				"name": "Haslyn Broberg",
+				"primary": "HASLYN_BROBERG.jpg",
+				"secondary": null,
+				"department": "Distribution",
+				"role": "Assembly"
+			},
+			{
+				"name": "Maria Carpenter",
+				"primary": "MARIA_CARPENTER.jpg",
+				"secondary": null,
+				"department": "Distribution",
+				"role": "Assembly"
+			},
+			{
+				"name": "Rosa Ornelas",
+				"primary": "ROSA_ORNELAS.jpg",
+				"secondary": null,
+				"department": "Distribution",
+				"role": "Assembly"
+			},
+			{
+				"name": "Jorge Nieves",
+				"primary": "JORGE_NIEVES.jpg",
+				"secondary": null,
+				"department": "Distribution",
+				"role": "Forklift Operator"
+			},
+			{
+				"name": "Matthew Hayre",
+				"primary": "MATT_HAYRE.jpg",
+				"secondary": null,
+				"department": "Distribution",
+				"role": "Inventory Lead"
+			},
+			{
+				"name": "Rafe Kiely",
+				"primary": "RAFE_KIELY.jpg",
+				"secondary": null,
+				"department": "Distribution",
+				"role": "Logistics Analyst"
+			},
+			{
+				"name": "David Carpenter",
+				"primary": "DAVID_CARPENTER.jpg",
+				"secondary": null,
+				"department": "Distribution",
+				"role": "Material Handler"
+			},
+			{
+				"name": "Fernando Hernandez",
+				"primary": "FERNANDO_HERNANDEZ.jpg",
+				"secondary": null,
+				"department": "Distribution",
+				"role": "Material Handler"
+			},
+			{
+				"name": "Jim Woosley",
+				"primary": "JIMMY_WOOSLEY.jpg",
+				"secondary": null,
+				"department": "Distribution",
+				"role": "Material Handler"
+			},
+			{
+				"name": "Jim Catalano",
+				"primary": "JAMES_CATALANO.jpg",
+				"secondary": null,
+				"department": "Distribution",
+				"role": "Senior Order Picker"
+			},
+			{
+				"name": "Ned Herbert",
+				"primary": "EDWARD_HERBERT.jpg",
+				"secondary": null,
+				"department": "Distribution",
+				"role": "Packer"
+			},
+			{
+				"name": "Alex Casimiro",
+				"primary": "ALEJANDRA_CASIMIRO.jpg",
+				"secondary": null,
+				"department": "Distribution",
+				"role": "Picker"
+			},
+			{
+				"name": "Cheilyn Villegas",
+				"primary": "CHEILYN_VILLEGAS.jpg",
+				"secondary": null,
+				"department": "Distribution",
+				"role": "Picker"
+			},
+			{
+				"name": "Dolores Escobedo",
+				"primary": "DOLORES_ESCOBEDO.jpg",
+				"secondary": null,
+				"department": "Distribution",
+				"role": "Picker"
+			},
+			{
+				"name": "Jessica Sanchez",
+				"primary": "JESSICA_SANCHEZ.jpg",
+				"secondary": null,
+				"department": "Distribution",
+				"role": "Picker"
+			},
+			{
+				"name": "Juana Moreno",
+				"primary": "JUANA_MORENO.jpg",
+				"secondary": null,
+				"department": "Distribution",
+				"role": "Picker"
+			},
+			{
+				"name": "Julia Amado",
+				"primary": "JULIA_AMADO.jpg",
+				"secondary": null,
+				"department": "Distribution",
+				"role": "Picker"
+			},
+			{
+				"name": "Luis Diaz",
+				"primary": "LUIS_DIAZ.jpg",
+				"secondary": null,
+				"department": "Distribution",
+				"role": "Picker"
+			},
+			{
+				"name": "Rosemary Aguilar",
+				"primary": "ROSEMARY_AGUILAR.jpg",
+				"secondary": null,
+				"department": "Distribution",
+				"role": "Picker"
+			},
+			{
+				"name": "Susan Urbin",
+				"primary": "SUSAN_URBIN.jpg",
+				"secondary": null,
+				"department": "Distribution",
+				"role": "Picker"
+			},
+			{
+				"name": "Laurie Wieringa",
+				"primary": "LAURIE_WIERINGA.jpg",
+				"secondary": null,
+				"department": "Distribution",
+				"role": "Receiving"
+			},
+			{
+				"name": "Dan Clawson",
+				"primary": "DAN_CLAWSON.jpg",
+				"secondary": null,
+				"department": "Distribution",
+				"role": "Shipping"
+			},
+			{
+				"name": "Myriam Oquendo",
+				"primary": "MYRIAM_OQUENDO.jpg",
+				"secondary": null,
+				"department": "Distribution",
+				"role": "Shipping"
+			},
+			{
+				"name": "Nancy Kershaw",
+				"primary": "NANCY_KERSHAW.jpg",
+				"secondary": null,
+				"department": "Distribution",
+				"role": "Shipping"
+			}
+    	],
+    	"department": "Distribution"
+  	},
+	'marketing': {
+    	"employees": [
+			{
+				"name": "Lisa Rice",
+				"primary": "LISA_RICE.jpg",
+				"secondary": null,
+				"department": "Marketing",
+				"role": "Marketing Manager"
+			},
+			{
+				"name": "Scott Johnson",
+				"primary": "SCOTT_JOHNSON.jpg",
+				"secondary": null,
+				"department": "Marketing",
+				"role": "Marketing Intern"
+			}
+    	],
+    	"department": "Marketing"
+	},
+	'productDevelopment': {
+		"employees": [
+			{
+				"name": "Debbie Christensen",
+				"primary": "DEBBIE_CHRISTENSEN.jpg",
+				"secondary": null,
+				"department": "Product Development",
+				"role": "Proofreader / Editor"
+			},
+			{
+				"name": "Kathleen Kepley",
+				"primary": "KATHLEEN_KEPLEY.jpg",
+				"secondary": null,
+				"department": "Product Development",
+				"role": "Custom Project Coordinator"
+			},
+			/*{
+				"name": "Shelly Rose",
+				"primary": "SHELLY_ROSE.jpg",
+				"secondary": "SHELLY_ROSE_2.jpg",
+				"department": "Product Development",
+				"role": "Project Manager"
+			},*/
+			{
+				"name": "Vikki McCallister",
+				"primary": "VIKKI_McCallister.jpg",
+				"secondary": null,
+				"department": "Product Development",
+				"role": "Product Manager"
+			}
+		],
+		"department": "Product Development"
+	},
+	'sales': {
+		"employees": [
+			{
+				"name": "Jeff Francisco",
+				"primary": null,
+				"secondary": null,
+				"department": "Sales",
+				"role": "Sales Manager"
+			},
+			{
+				"name": "Shawn LeBar",
+				"primary": "SHAWN_LeBAR.jpg",
+				"secondary": null,
+				"department": "Sales",
+				"role": "Sales Manager"
+			}
+		],
+		"department": "Sales"
+	}
+}
