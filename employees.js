a78a9421781f77aa55d2e182ccc2be576b0c57ce
sjohnson-CAGifts <54120577+sjohnson-CAGifts@users.noreply.github.com>
2019-09-05 19:08:37 -0500

    Add files via upload

--- employees.js
+++ employees.js
@@ -6,42 +6,54 @@ var staff = {
 				"primary": "HEINRICH_JOHNSEN.jpg",
 				"secondary": null,
 				"department": "Administration",
-				"role": "President / Owner"
+				"role": "President / Owner",
+				"email": "hjohnsen@cagifts.com",
+				"extension": "1011"
 			},
 			{
 				"name": "CJ Van Wagner",
 				"primary": "CJ.jpg",
 				"secondary": "CJ_2.jpg",
 				"department": "Administration",
-				"role": "VP of People and Product Development"
+				"role": "VP of People and Product Development",
+				"email": "cjvanwagner@cagifts.com",
+				"extension": "1048"
 			},
 			{
 				"name": "Michael Hoving",
 				"primary": "MIKE_HOVING.jpg",
 				"secondary": null,
 				"department": "Administration",
-				"role": "VP of Finance and Operations"
+				"role": "VP of Finance and Operations",
+				"email": "mhoving@cagifts.com",
+				"extension": "1038"
 			},
 			{
 				"name": "Eden Parker",
 				"primary": "EDEN_PARKER.jpg",
 				"secondary": "EDEN_PARKER_2.jpg",
 				"department": "Administration",
-				"role": "Bibles.net Coordinator"
+				"role": "Bibles.net Coordinator",
+				"email": "eparker@cagifts.com",
+				"extension": "1011"
 			},
 			{
 				"name": "Susan Middel",
 				"primary": "SUSAN_MIDDEL.jpg",
 				"secondary": null,
 				"department": "Administration",
-				"role": "Administrative Assistant"
+				"role": "Administrative Assistant",
+				"email": "smiddel@cagifts.com",
+				"extension": "1028"
 			},
 			{
 				"name": "Trevor Heinisch",
 				"primary": "TREVOR_HEINISCH.jpg",
 				"secondary": null,
 				"department": "Administration",
-				"role": "Information Technology Manager"
+				"role": "Information Technology Manager",
+				"email": "theinisch@cagifts.com",
+				"extension": "1016"
 	  		}
 	    ],
     	"department": "Administration"
@@ -49,18 +61,22 @@ var staff = {
 	'accounting': {
     	"employees": [
 			{
-	        	"name": "Evelyn Szczepanik",
-	        	"primary": "EVELYN_SZXZEPANIK.jpg",
-	        	"secondary": null,
-	        	"department": "Accounting",
-	    		"role": "Accounts Receivable"
+        	"name": "Evelyn Szczepanik",
+        	"primary": "EVELYN_SZXZEPANIK.jpg",
+        	"secondary": null,
+        	"department": "Accounting",
+	    		"role": "Accounts Receivable",
+					"email": "eszczepanik@cagifts.com",
+					"extension": "1040"
 			},
 			{
 				"name": "Lettie Van Wyk",
 				"primary": "LETTIE_VanWyk.jpg",
 				"secondary": null,
 				"department": "Accounting",
-				"role": "Accounting Assistant"
+				"role": "Accounting Assistant",
+				"email": "accounting@cagifts.com",
+				"extension": "1018"
 			}
     	],
     	"department": "Accounting"
@@ -72,42 +88,53 @@ var staff = {
 				"primary": "BRAD_MIEDEMA.jpg",
 				"secondary": null,
 				"department": "Creative",
-				"role": "Art Director"
+				"role": "Art Director",
+				"email": "bmiedema@cagifts.com",
+				"extension": "1022"
 			},
 			{
 				"name": "Allison Sowers",
 				"primary": "ALLISON_SOWERS.jpg",
 				"secondary": null,
 				"department": "Creative",
-				"role": "Designer"
+				"role": "Designer",
+				"email": "asowers@cagifts.com",
+				"extension": "1032"
 			},
 			{
 				"name": "Erin Simula",
 				"primary": "ERIN_SIMULA.jpg",
 				"secondary": null,
 				"department": "Creative",
-				"role": "Designer"
+				"role": "Designer",
+				"email": "esimula@cagifts.com",
+				"extension": "1024"
 			},
 			{
 				"name": "Nicole Grimes",
 				"primary": null,
 				"secondary": null,
 				"department": "Creative",
-				"role": "Designer"
+				"role": "Designer",
+				"email": "ngrimes@cagfits.com"
 			},
 			{
 				"name": "Staci Bryant",
 				"primary": "STACI_BRYANT_2.jpg",
 				"secondary": null,
 				"department": "Creative",
-				"role": "Designer / Superwoman"
+				"role": "Designer / Superwoman",
+				"email": "sbryant@cagifts.com",
+				"extension": "1021"
 			},
 			{
 				"name": "Greg Heck",
 				"primary": "GREG_HECK.jpg",
 				"secondary": null,
 				"department": "Creative",
-				"role": "Photographer"
+				"role": "Photographer",
+				"email": "gheck@cagifts.com",
+				"extension": "1027"
 			}
     	],
     	"department": "Creative"
@@ -115,32 +142,40 @@ var staff = {
 	'customerService': {
     	"employees": [
 			{
+				"name": "Kelly Glancey",
+				"primary": "KELLY_GLANCEY.jpg",
+				"secondary": null,
+				"department": "Customer Service",
+				"role": "Customer Service Key Accounts",
+				"email": "kglancey@cagifts.com",
+				"extension": "1013"
+			},
+			{
 				"name": "Christina Okesson",
 				"primary": "CHRISTINA_OKESSON.jpg",
 				"secondary": null,
 				"department": "Customer Service",
-				"role": "Customer Service Representative"
+				"role": "Customer Service Representative",
+				"email": "cokesson@cagifts.com",
+				"extension": "1045"
 			},
 			{
 				"name": "Erica Yoneyama",
 				"primary": "ERICA_YONEYAMA.jpg",
 				"secondary": null,
 				"department": "Customer Service",
-				"role": "Customer Service Representative"
+				"role": "Customer Service Representative",
+				"email": "eyoneyama@cagifts.com",
+				"extension": "1029"
 			},
 			{
 				"name": "Jim Snyder",
 				"primary": "JAMES_SNYDER.jpg",
 				"secondary": null,
 				"department": "Customer Service",
-				"role": "Customer Service Representative"
-			},
-			{
-				"name": "Kelly Glancey",
-				"primary": "KELLY_GLANCEY.jpg",
-				"secondary": null,
-				"department": "Customer Service",
-				"role": "Key Accounts Manager"
+				"role": "Customer Service Representative",
+				"email": "jsnyder@cagifts.com",
+				"extension": "1025"
 			}
     	],
     	"department": "Customer Service"
@@ -152,175 +187,187 @@ var staff = {
 				"primary": "RON_WISSER_2.jpg",
 				"secondary": "RON_WISSER.jpg",
 				"department": "Distribution",
-				"role": "Distribution Manager"
-			},
-			{
-				"name": "Haslyn Broberg",
-				"primary": "HASLYN_BROBERG.jpg",
-				"secondary": null,
-				"department": "Distribution",
-				"role": "Assembly"
-			},
-			{
-				"name": "Maria Carpenter",
-				"primary": "MARIA_CARPENTER.jpg",
-				"secondary": null,
-				"department": "Distribution",
-				"role": "Assembly"
-			},
-			{
-				"name": "Rosa Ornelas",
-				"primary": "ROSA_ORNELAS.jpg",
-				"secondary": null,
-				"department": "Distribution",
-				"role": "Assembly"
-			},
-			{
-				"name": "Jorge Nieves",
-				"primary": "JORGE_NIEVES.jpg",
-				"secondary": null,
-				"department": "Distribution",
-				"role": "Forklift Operator"
-			},
-			{
-				"name": "Matthew Hayre",
-				"primary": "MATT_HAYRE.jpg",
-				"secondary": null,
-				"department": "Distribution",
-				"role": "Inventory Lead"
+				"role": "Logistics Manager",
+				"email": "rwisser@cagifts.com",
+				"extension": "1034"
 			},
 			{
 				"name": "Rafe Kiely",
 				"primary": "RAFE_KIELY.jpg",
 				"secondary": null,
 				"department": "Distribution",
-				"role": "Logistics Analyst"
-			},
-			{
-				"name": "David Carpenter",
-				"primary": "DAVID_CARPENTER.jpg",
-				"secondary": null,
-				"department": "Distribution",
-				"role": "Material Handler"
-			},
-			{
-				"name": "Fernando Hernandez",
-				"primary": "FERNANDO_HERNANDEZ.jpg",
-				"secondary": null,
-				"department": "Distribution",
-				"role": "Material Handler"
-			},
-			{
-				"name": "Jim Woosley",
-				"primary": "JIMMY_WOOSLEY.jpg",
-				"secondary": null,
-				"department": "Distribution",
-				"role": "Material Handler"
-			},
-			{
-				"name": "Jim Catalano",
-				"primary": "JAMES_CATALANO.jpg",
-				"secondary": null,
-				"department": "Distribution",
-				"role": "Senior Order Picker"
-			},
-			{
-				"name": "Ned Herbert",
-				"primary": "EDWARD_HERBERT.jpg",
-				"secondary": null,
-				"department": "Distribution",
-				"role": "Packer"
-			},
-			{
-				"name": "Alex Casimiro",
-				"primary": "ALEJANDRA_CASIMIRO.jpg",
-				"secondary": null,
-				"department": "Distribution",
-				"role": "Picker"
-			},
-			{
-				"name": "Cheilyn Villegas",
-				"primary": "CHEILYN_VILLEGAS.jpg",
-				"secondary": null,
-				"department": "Distribution",
-				"role": "Picker"
-			},
-			{
-				"name": "Dolores Escobedo",
-				"primary": "DOLORES_ESCOBEDO.jpg",
-				"secondary": null,
-				"department": "Distribution",
-				"role": "Picker"
-			},
-			{
-				"name": "Jessica Sanchez",
-				"primary": "JESSICA_SANCHEZ.jpg",
-				"secondary": null,
-				"department": "Distribution",
-				"role": "Picker"
+				"role": "Logistics Supervisor",
+				"email": "rkiely@cagifts.com",
+				"extension": "1015"
 			},
 			{
 				"name": "Juana Moreno",
 				"primary": "JUANA_MORENO.jpg",
 				"secondary": null,
 				"department": "Distribution",
-				"role": "Picker"
+				"role": "Fulfillment Lead",
+				"email": "jmoreno@cagifts.com",
+				"extension": "1023"
+			},
+			{
+				"name": "Cheilyn Villegas",
+				"primary": "CHEILYN_VILLEGAS.jpg",
+				"secondary": null,
+				"department": "Distribution",
+				"role": "Fulfillment Technician"
 			},
 			{
 				"name": "Julia Amado",
 				"primary": "JULIA_AMADO.jpg",
 				"secondary": null,
 				"department": "Distribution",
-				"role": "Picker"
+				"role": "Fulfillment Technician"
 			},
 			{
 				"name": "Luis Diaz",
 				"primary": "LUIS_DIAZ.jpg",
 				"secondary": null,
 				"department": "Distribution",
-				"role": "Picker"
+				"role": "Fulfillment Technician"
 			},
 			{
 				"name": "Rosemary Aguilar",
 				"primary": "ROSEMARY_AGUILAR.jpg",
 				"secondary": null,
 				"department": "Distribution",
-				"role": "Picker"
+				"role": "Fulfillment Technician"
 			},
 			{
 				"name": "Susan Urbin",
 				"primary": "SUSAN_URBIN.jpg",
 				"secondary": null,
 				"department": "Distribution",
-				"role": "Picker"
+				"role": "Fulfillment Technician"
 			},
 			{
-				"name": "Laurie Wieringa",
-				"primary": "LAURIE_WIERINGA.jpg",
+				"name": "Alex Casimiro",
+				"primary": "ALEJANDRA_CASIMIRO.jpg",
 				"secondary": null,
 				"department": "Distribution",
-				"role": "Receiving"
+				"role": "Part-Time Fulfillment Technician"
+			},
+			{
+				"name": "Dolores Escobedo",
+				"primary": "DOLORES_ESCOBEDO.jpg",
+				"secondary": null,
+				"department": "Distribution",
+				"role": "Part-Time Fulfillment Technician"
+			},
+			{
+				"name": "Jessica Sanchez",
+				"primary": "JESSICA_SANCHEZ.jpg",
+				"secondary": null,
+				"department": "Distribution",
+				"role": "Part-Time Fulfillment Technician"
+			},
+			{
+				"name": "Jim Catalano",
+				"primary": "JAMES_CATALANO.jpg",
+				"secondary": null,
+				"department": "Distribution",
+				"role": "Part-Time Fulfillment Technician"
+			},
+			{
+				"name": "Maria Carpenter",
+				"primary": "MARIA_CARPENTER.jpg",
+				"secondary": null,
+				"department": "Distribution",
+				"role": "Assembly Technician"
+			},
+			{
+				"name": "Haslyn Broberg",
+				"primary": "HASLYN_BROBERG.jpg",
+				"secondary": null,
+				"department": "Distribution",
+				"role": "Part-Time Assembly Technician"
+			},
+			{
+				"name": "Rosa Ornelas",
+				"primary": "ROSA_ORNELAS.jpg",
+				"secondary": null,
+				"department": "Distribution",
+				"role": "Part-Time Assembly Technician"
+			},
+			{
+				"name": "Matthew Hayre",
+				"primary": "MATT_HAYRE.jpg",
+				"secondary": null,
+				"department": "Distribution",
+				"role": "Inventory Lead",
+				"email": "mhayre@cagifts.com"
 			},
 			{
 				"name": "Dan Clawson",
 				"primary": "DAN_CLAWSON.jpg",
 				"secondary": null,
 				"department": "Distribution",
-				"role": "Shipping"
+				"role": "Inventory Technician"
 			},
 			{
-				"name": "Myriam Oquendo",
-				"primary": "MYRIAM_OQUENDO.jpg",
+				"name": "David Carpenter",
+				"primary": "DAVID_CARPENTER.jpg",
 				"secondary": null,
 				"department": "Distribution",
-				"role": "Shipping"
+				"role": "Inventory Technician"
+			},
+			{
+				"name": "Jim Woosley",
+				"primary": "JIMMY_WOOSLEY.jpg",
+				"secondary": null,
+				"department": "Distribution",
+				"role": "Inventory Technician"
+			},
+			{
+				"name": "Jorge Nieves",
+				"primary": "JORGE_NIEVES.jpg",
+				"secondary": null,
+				"department": "Distribution",
+				"role": "Inventory Technician"
+			},
+			{
+				"name": "Laurie Wieringa",
+				"primary": "LAURIE_WIERINGA.jpg",
+				"secondary": null,
+				"department": "Distribution",
+				"role": "Receiving Lead",
+				"email": "lwieringa@cagifts.com",
+				"extension": "1045"
 			},
 			{
 				"name": "Nancy Kershaw",
 				"primary": "NANCY_KERSHAW.jpg",
 				"secondary": null,
 				"department": "Distribution",
-				"role": "Shipping"
+				"role": "Shipping Lead",
+				"email": "nkershaw@cagifts.com",
+				"extension": "1030"
+			},
+			{
+				"name": "Ned Herbert",
+				"primary": "EDWARD_HERBERT.jpg",
+				"secondary": null,
+				"department": "Distribution",
+				"role": "Meter Operator"
+			},
+			{
+				"name": "Myriam Oquendo",
+				"primary": "MYRIAM_OQUENDO.jpg",
+				"secondary": null,
+				"department": "Distribution",
+				"role": "Shipping Associate",
+				"extension": "1017"
+			},
+			{
+				"name": "Fernando Hernandez",
+				"primary": "FERNANDO_HERNANDEZ.jpg",
+				"secondary": null,
+				"department": "Distribution",
+				"role": "Part-Time Shipping Associate"
 			}
     	],
     	"department": "Distribution"
@@ -332,14 +379,18 @@ var staff = {
 				"primary": "LISA_RICE.jpg",
 				"secondary": null,
 				"department": "Marketing",
-				"role": "Marketing Manager"
+				"role": "Marketing Manager",
+				"email": "lrice@cagifts.com",
+				"email_alt": "lisa@inspirational@cagifts.com",
+				"extension": "1026"
 			},
 			{
 				"name": "Scott Johnson",
 				"primary": "SCOTT_JOHNSON.jpg",
 				"secondary": null,
 				"department": "Marketing",
-				"role": "Marketing Intern"
+				"role": "Marketing Intern",
+				"email": "sjohnson@cagifts.com"
 			}
     	],
     	"department": "Marketing"
@@ -351,14 +402,18 @@ var staff = {
 				"primary": "DEBBIE_CHRISTENSEN.jpg",
 				"secondary": null,
 				"department": "Product Development",
-				"role": "Proofreader / Editor"
+				"role": "Proofreader / Editor",
+				"email": "dchristensen@cagifts.com",
+				"extension": "1014"
 			},
 			{
 				"name": "Kathleen Kepley",
 				"primary": "KATHLEEN_KEPLEY.jpg",
 				"secondary": null,
 				"department": "Product Development",
-				"role": "Custom Project Coordinator"
+				"role": "Custom Project Coordinator",
+				"email": "kkepley@cagifts.com",
+				"extension": "1037"
 			},
 			/*{
 				"name": "Shelly Rose",
@@ -372,7 +427,9 @@ var staff = {
 				"primary": "VIKKI_McCallister.jpg",
 				"secondary": null,
 				"department": "Product Development",
-				"role": "Product Manager"
+				"role": "Product Manager",
+				"email": "vmccallister@cagifts.com",
+				"extension": "1046"
 			}
 		],
 		"department": "Product Development"
@@ -384,14 +441,18 @@ var staff = {
 				"primary": null,
 				"secondary": null,
 				"department": "Sales",
-				"role": "Sales Manager"
+				"role": "Sales Manager",
+				"email": "jfrancisco@cagifts.com",
+				"cell": "918-850-8007"
 			},
 			{
 				"name": "Shawn LeBar",
 				"primary": "SHAWN_LeBAR.jpg",
 				"secondary": null,
 				"department": "Sales",
-				"role": "Sales Manager"
+				"role": "Sales Manager",
+				"email": "slebar@cagifts.com",
+				"cell": "918-513-1367"
 			}
 		],
 		"department": "Sales"
